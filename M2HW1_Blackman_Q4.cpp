8ee591c5738708e97c087649f493c34455da92bc
Tim <blackmantjames@gmail.com>
2025-02-17 04:47:24 +0000

    Save

---
+++ M2HW1_Blackman_Q4.cpp
@@ -0,0 +1,25 @@
+//M2HW1_Q4
+//Blackman, James
+//16 Feb 25
+//Cheering 
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    string school = "FTCC!";
+    string team = "Trojans!";
+    string chant ="Let's go"
+
+    // Output cheering messages using variables
+    cout << chant << school << endl;
+    cout <<chant << school << endl;
+    cout << chant << school << endl;
+    cout << chant << team << endl;
+
+    return 0;
+}
