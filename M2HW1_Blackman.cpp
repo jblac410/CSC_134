01c8cc04323b38772b2ed1fb755dfae35071ff66
Tim <blackmantjames@gmail.com>
2025-02-17 01:31:52 +0000

    save

---
+++ M2HW1_Blackman.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>  // For formatting decimal places
+
+using namespace std;
+
+int main() {
+string name;
+double starting_balance, deposit_amount, final_balance;
+cout<< "Enter your full name: ";
+getline(cin, name);
+cout << "Enter your starting balance ($): ";
+
+
+
+
+}
