af615b9d33905ce9915b5216545d8922ba0d4052
Tim <blackmantjames@gmail.com>
2025-02-26 15:34:38 +0000

    if else if

---
+++ M3Lab2_Blackman.cpp
@@ -0,0 +1,44 @@
+// CSC 134
+// M3Lab2
+// Blackman, James
+// 3/26/2025
+// Grade Calculator
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+int num_grade;
+string letter_grade;
+
+cout<< "Number grade to letter grade conversion program." << endl;
+cout<< "Enter the number grade: ";
+cin >> num_grade;
+
+cout <<"The grade " << num_grade<< " is a " << letter_grade << endl;
+if (num_grade = 90 >= 100)
+{ 
+cout << "Great job, you got an A! " << endl;
+}
+
+else if (num_grade = 80 >= 89)
+{
+    cout << "Good job, you got an B! " << endl;
+
+
+}
+
+return 0; 
+}
+
+
+
+
+
+
+
+
+
+}
