7500d145bf739631bcdc4fb2934dbdc3b80abe84
Tim <blackmantjames@gmail.com>
2025-03-10 14:47:53 +0000

    M4T1

---
+++ M4T1_Blackman.cpp
@@ -0,0 +1,34 @@
+//CSC 134
+//M4T1
+// Blackman
+// 10 March 2025
+// Table of squares
+
+#include <iostream>
+using namespace std; 
+
+
+
+int main()
+{
+    while (count <= 5) 
+    {
+        cout << "Done" << endl;
+        const int MIN_NUM =1
+        const int MAX_NUM = 10;
+        cout << "Table of Squares" << endl;
+        cout << "----------------" << endl;
+        int = MIN_NUM;
+        int = sq;
+        
+       
+             while(num <= MAX_NUM)
+                {
+                sq = num* num;
+                cout << num<< "\t" << sq << endl;
+                num = num + 1;
+
+                }
+    }
+return 0;    
+}
