d8916dcd37ab6722a2ab0822d9c7aefa3a744db2
Tim <blackmantjames@gmail.com>
2025-03-21 20:11:42 +0000

    finished and prints as it should

--- M4Lab1_Blackman.cpp
+++ M4Lab1_Blackman.cpp
@@ -7,56 +7,60 @@
 
 #include <iostream>
 using namespace std;
-int main()
-{
-string pixel ="🚲"; //copy a unicode symbol as the symbol not the code.
- int WIDTH = 5;
- int HEIGHT = 5;
 
-cout << "Enter Width: " << endl;
-cin >> WIDTH   ; 
+int main() {
+  // New info discovered:
+  // Any regular character can be char
+  // but Unicode characters have to be strings
+  string pixel = "🚲"; // use any one character or an emoji
+  const int WIDTH = 8;
+  const int HEIGHT = 5;
+  cout << pixel << endl;
 
-cout << "Enter Height: " << endl;
-cin >> HEIGHT ; 
+  // three ways to run a loop
+  // 1. while loop
+  int count = 0;
+  while (count < 5) {
+    cout << pixel;
+    count++;
+  }
+  cout << endl;
 
-cout << pixel << endl;
-int count = 0;
-while (count < 5) //while loop
-{
-    cout<<pixel;
-    count++; 
-
-}
-count = 0;
-do 
-{
+  // 2. do while loop
+  count = 0;
+  do {
     cout << pixel << " ";
     count++;
-    
-}
-while (count < 5);
-for (int i=0; i < 10; i++);
-{
+  } while (count < 5);
+  cout << endl;
+
+  // 3. for loop
+  // This is the one time using i as a variable name is ok
+  // stands for index
+  cout << endl;
+  cout << pixel << " printed horizontally (row)" << endl;
+  for (int i = 0; i < WIDTH; i++) {
+    cout << pixel << " ";
+  }
+  cout << endl;
+
+  cout << pixel << " printed vertically (column)" << endl;
+  for (int i = 0; i < HEIGHT; i++) {
+    cout << pixel << endl;
+  }
+  cout << endl;
+
+  cout << "Printing a box " << WIDTH << " by " << HEIGHT << endl;
+  // this is a nested loop
+  // for each row
+  for (int h = 0; h < HEIGHT; h++) {
+    // print the current row
+    for (int w = 0; w < WIDTH; w++) {
+      cout << pixel << " ";
+    }
+    // go to the next line
     cout << endl;
-cout << pixel << " printed horizontally  (row) " <<endl;
-for (int i = 0; i < WIDTH; i++)
-cout << pixel << " ";
-
-cout << endl;
-cout << pixel << " printed vertically (column) " <<endl;
-for (int i = 0; i < HEIGHT; i++)
-cout << pixel << " " << endl;
-
-}
-cout << endl << "Printing a box" << WIDTH << " by " << HEIGHT << endl;
-for ( int w=0; w < WIDTH; w++ )
-{
-    cout << pixel << " " << endl;
-
-
-}
-cout << endl;
-
-
-return 0; 
+  }
+  cout << endl;
+  return 0;
 }
