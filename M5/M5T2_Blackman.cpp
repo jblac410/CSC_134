f54b8196ef0be97688e115eb6e9f69f7e8d10ff8
Tim <blackmantjames@gmail.com>
2025-03-26 15:06:05 +0000

    changed method

--- M5/M5T2_Blackman.cpp
+++ M5/M5T2_Blackman.cpp
@@ -5,16 +5,33 @@
 
 // create 1 void function and 1 value returning funtion
 
-#include <iostream>
+#include <iostream> 
+#include <cmath>
 using namespace std;
 
 // declare void functions
-void first_message();
-void greet_user(string name);
+
+/*a main() function
+a square() function that takes an int, and returns the square of that int (value returning)
+a printAnswerLine() function that prints one line of the answer (void)*/
+int square(int number);
+void printAnswerLine();
 int double_a_number(int number);
 
 int main() {
+    cout << "Lets colab!" << endl;
+    int number;
+    cin >> number;
+    int square_1 = square (square_1);
+    cout << "The square of " << square_1  << std::endl;
 
 
-    
+}
+
+int square(int number);
+{
+
+int square_1; 
+square_1 = number * number;
+return square_1; 
 }
