62aefea3a18870aa29217a1a9ba583ff2d4e2b93
Tim <blackmantjames@gmail.com>
2025-03-30 21:09:23 +0000

    started over to simplify.

--- M4HW1_Blackman2.cpp
+++ M4HW1_Blackman2.cpp
@@ -4,11 +4,30 @@
 // 30/03/2025
 //Gold
 
-#include <iostream>
 
+    #include <iostream>
 using namespace std;
 
 int main() {
+    int m_table;
 
-    
+    // Input validation loop
+    do {
+        cout << "Enter a number from 1 to 12: ";
+        cin >> m_table;
+
+        if (m_table < 1 || m_table > 12) { //Or operator 
+            cout << "Invalid input. Please try again." << endl;
+        }
+
+    } while (m_table < 1 || m_table > 12);
+
+    // Multiplication table loop
+    int num = 1;
+    while (num <= 12) {
+        cout << m_table << " times " << num << " is " << m_table * num << "." << endl;
+        num++; //adding one until <=12
+    }
+
+    return 0;
 }
