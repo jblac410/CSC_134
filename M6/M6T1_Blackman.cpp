a9bb840ba9ccb10553c65022893d7206d8d37701
Tim <blackmantjames@gmail.com>
2025-04-07 15:04:55 +0000

    strange numbers

--- M6/M6T1_Blackman.cpp
+++ M6/M6T1_Blackman.cpp
@@ -7,14 +7,21 @@ int main()
     int cars_today;
     int cars_total = 0;
     double cars_average;
-
+int cars [DAYS];
     cout <<"Enter cars that pass for each day." << endl;
-    for(int i=0; 1<DAYS; i++)
+    for(int i=0; i<DAYS; i++)
     {
         cout << "Day" << i+1 << ": ";
         cin >> cars_today;
         cars_total += cars_today;
     }
     cout<< "Over "<< DAYS << " days, total cars = "<< cars_total<< endl;
+    cars_average = (double) cars_total / DAYS;
+    cout << "For and average of " << cars_average << "each day." << endl;
 
+    for(int i=0;    i <DAYS; i++)
+    {
+        cout << "Day: " << i+1 << "Count: " << cars[i] << endl;
+        
+    }
 }
